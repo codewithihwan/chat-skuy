b742787fa778bc42e8b7446782071820406f7da6
Ihwan <ihwan@indosystem.com>
2019-10-20 14:59:42 +0700

    design welcome screen

--- lib/ui/welcome_screen.dart
+++ lib/ui/welcome_screen.dart
@@ -12,7 +12,37 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
       appBar: AppBar(
         title: Text('Welcome'),
       ),
-      body: Center(child: Text('Welcome Bro')),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            MaterialButton(
+                minWidth: 200.0,
+                height: 42.0,
+                child: Text(
+                  'Register',
+                  style: TextStyle(color: Colors.white),
+                ),
+                color: Colors.blue,
+                onPressed: () {
+                  Navigator.pushNamed(context, "register");
+                }),
+            MaterialButton(
+                minWidth: 200.0,
+                height: 42.0,
+                child: Text(
+                  'Login',
+                  style: TextStyle(color: Colors.white),
+                ),
+                color: Colors.blue,
+                onPressed: () {
+                  Navigator.pushNamed(context, "/login");
+                }),
+          ],
+        ),
+      ),
     );
   }
 }
