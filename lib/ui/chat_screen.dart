b1e09bf79d755a21c21906175a024ea25789e768
Ihwan <ihwan@indosystem.com>
2019-10-20 15:50:40 +0700

    design ui for chat screen

--- lib/ui/chat_screen.dart
+++ lib/ui/chat_screen.dart
@@ -8,6 +8,46 @@ class ChatScreen extends StatefulWidget {
 class _ChatScreenState extends State<ChatScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        leading: Container(),
+        title: Text(
+          'Chat Skuy',
+        ),
+        actions: <Widget>[
+          IconButton(
+              icon: Icon(Icons.close),
+              onPressed: () {
+                Navigator.pop(context);
+              }),
+        ],
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Expanded(
+            child: Container(),
+          ),
+          Container(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      contentPadding: EdgeInsets.symmetric(
+                          vertical: 10.0, horizontal: 20.0),
+                      hintText: 'Type your message here...',
+                      border: InputBorder.none,
+                    ),
+                  ),
+                ),
+                FlatButton(onPressed: () {}, child: Text('send'))
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
